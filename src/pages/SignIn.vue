9b0aa9539a138ede348ea0032c61cf8cbe13bef4
Brian <briancbarrow@gmail.com>
2021-04-15 10:05:19 -0600

    added auth0, still need to protect routes and redirect properly

---
+++ src/pages/SignIn.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page
+    class="bg-light-green window-height window-width row justify-center items-center"
+  >
+    <div class="column">
+      <div class="row">
+        <h5 class="text-h5 text-white q-my-md">Booklist App</h5>
+      </div>
+      <div class="row">
+        <q-card square bordered class="q-pa-lg shadow-1">
+          <q-card-section>
+            <!-- <q-form class="q-gutter-md">
+              <q-input
+                square
+                filled
+                clearable
+                v-model="email"
+                type="email"
+                label="email"
+              />
+              <q-input
+                square
+                filled
+                clearable
+                v-model="password"
+                type="password"
+                label="password"
+              />
+            </q-form> -->
+            <div v-if="!$auth.loading">IS NOT LOADING</div>
+            <div v-if="$auth.loading">IS LOADING</div>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn
+              unelevated
+              color="light-green-7"
+              size="lg"
+              class="full-width"
+              label="Login"
+              v-if="!$auth.isAuthenticated"
+              @click="login"
+            />
+            <q-btn
+              unelevated
+              color="light-green-7"
+              size="lg"
+              class="full-width"
+              label="Logout"
+              v-if="$auth.isAuthenticated"
+              @click="logout"
+            />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-none">
+            <p class="text-grey-6">Not reigistered? Create an Account</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,24 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/dashboard",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: '/lists', component: () => import('pages/Lists.vue') }
+      { path: "", component: () => import("pages/Index.vue") },
+      { path: "/lists", component: () => import("pages/Lists.vue") }
     ]
+  },
+  {
+    path: "/",
+    component: () => import("layouts/NoAuth.vue"),
+    children: [{ path: "/", component: () => import("pages/SignIn.vue") }]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '*',
-    component: () => import('pages/Error404.vue')
+    path: "*",
+    component: () => import("pages/Error404.vue")
   }
-]
+];
 
-export default routes
+export default routes;
