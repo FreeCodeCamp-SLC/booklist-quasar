358384d3b4e8d8237252f6f1559bf3d1e5e42978
Peter Barrow <peterbarrow@Peters-MacBook-Pro.local>
2021-06-01 18:45:34 -0600

    Changed port to 3000

--- quasar.conf.js
+++ quasar.conf.js
@@ -70,7 +70,7 @@ module.exports = function(/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: true,
-      port: 8080,
+      port: 3000,
       open: true // opens browser window automatically
     },
 
